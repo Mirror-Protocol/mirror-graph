68f133db1ae798df9053123b6246a66910ca1b35
Alex Kim <alex@terra.money>
2020-10-07 16:29:00 +0900

    fix(collector): error handling

--- src/collector/collect.ts
+++ src/collector/collect.ts
@@ -2,18 +2,19 @@ import { format } from 'date-fns'
 import { getManager, EntityManager } from 'typeorm'
 import { getLatestBlockHeight, getTxs } from 'lib/terra'
 import * as logger from 'lib/logger'
+import { errorHandler } from 'lib/error'
 import { parseTxs } from './parser'
 import { getCollectedHeight, updateBlock } from './block'
 import config from 'config'
 
 export async function collect(now: number): Promise<void> {
-  const latestHeight = await getLatestBlockHeight()
+  const latestHeight = await getLatestBlockHeight().catch(errorHandler)
   const collectedHeight = await getCollectedHeight()
   if (!latestHeight || collectedHeight >= latestHeight) {
     return
   }
 
-  const txs = await getTxs(collectedHeight + 1, latestHeight, 500)
+  const txs = await getTxs(collectedHeight + 1, latestHeight, 500).catch(errorHandler)
   if (!txs || txs.length < 1) {
     return
   }
