4dc805bfdec826bf384e04a663b26e4b94572a99
Alex Kim <alex@terra.money>
2020-10-15 17:13:07 +0900

    fix: print log date with timezone

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,13 +1,22 @@
-import { format } from 'date-fns'
+import { formatToTimeZone } from 'date-fns-timezone'
 
 export function info(...args): void {
-  console.info(format(new Date(), 'yyyy-MM-dd HH:mm:ss'), ...args)
+  console.info(
+    formatToTimeZone(new Date(), 'YYYY-MM-DD HH:mm:ss', { timeZone: 'Asia/Seoul'}),
+    ...args
+  )
 }
 
 export function warn(...args): void {
-  console.warn(format(new Date(), 'yyyy-MM-dd HH:mm:ss'), ...args)
+  console.warn(
+    formatToTimeZone(new Date(), 'YYYY-MM-DD HH:mm:ss', { timeZone: 'Asia/Seoul'}),
+    ...args
+  )
 }
 
 export function error(...args): void {
-  console.error(format(new Date(), 'yyyy-MM-dd HH:mm:ss'), ...args)
+  console.error(
+    formatToTimeZone(new Date(), 'YYYY-MM-DD HH:mm:ss', { timeZone: 'Asia/Seoul'}),
+    ...args
+  )
 }
