9e9536655e4429f6007ad05348c686fc14a8d707
Alex Kim <alex@terra.money>
2020-10-28 17:50:50 +0900

    fix: add offset/limit args to txs query

--- src/graphql/resolvers/TxResolver.ts
+++ src/graphql/resolvers/TxResolver.ts
@@ -7,10 +7,16 @@ export class TxResolver {
   constructor(private readonly txService: TxService) {}
 
   @Query((returns) => [Tx])
-  async txs(@Arg('account') account: string): Promise<Tx[]> {
+  async txs(
+    @Arg('account') account: string,
+    @Arg('offset', { defaultValue: 0 }) offset: number,
+    @Arg('limit', { defaultValue: 100 }) limit: number,
+  ): Promise<Tx[]> {
     return this.txService.getAll({
       where: { address: account },
-      order: { id: 'DESC' }
+      order: { id: 'DESC' },
+      skip: offset,
+      take: limit,
     })
   }
 }
