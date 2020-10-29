bdc01db27538f8505614a413781de782a5b19a53
Alex Kim <alex@terra.money>
2020-10-29 17:22:19 +0900

    fix: calculate balance history

--- src/orm/functions/balanceHistory.sql
+++ src/orm/functions/balanceHistory.sql
@@ -1,7 +1,7 @@
 drop function public.balanceHistory;
 
 CREATE OR REPLACE FUNCTION public.balanceHistory(_address varchar, _from timestamp, _to timestamp, _interval integer)
-  RETURNS TABLE ("timestamp" timestamp, "value" numeric)
+  RETURNS TABLE ("timestamp" timestamp, "assetValue" numeric, "investedValue" numeric)
   LANGUAGE 'plpgsql'
   VOLATILE 
 AS $BODY$
@@ -13,16 +13,20 @@ BEGIN
     timeIteratorNext := timeIterator + (_interval * interval '1 minute');
 
     RETURN QUERY
-    SELECT timeIterator as "timestamp", coalesce(sum(pb.price*pb.balance), 0) as "value" FROM (
+    SELECT
+      timeIterator as "timestamp",
+      sum(pb.assetValue) as "assetValue",
+      sum(pb.investedValue) as "investedValue"
+    FROM (
       SELECT
         DISTINCT ON (token) token,
-        (SELECT p.close FROM price p
+        coalesce((SELECT p.close FROM price p
           WHERE p.token = b.token AND p.datetime <= timeIteratorNext
-          ORDER BY p.datetime DESC LIMIT 1) as price,
-        b.balance
-        FROM balance b
-        WHERE b.address=_address AND b.datetime <= timeIteratorNext
-        ORDER BY token, id DESC
+          ORDER BY p.datetime DESC LIMIT 1), 0)*b.balance as assetValue,
+        b.average_price*b.balance as investedValue
+      FROM balance b
+      WHERE b.address=_address AND b.datetime <= timeIteratorNext
+      ORDER BY token, id DESC
     ) as pb;
 
     EXIT WHEN timeIterator >= _to;
