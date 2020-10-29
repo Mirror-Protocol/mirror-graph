26501276cfc58c916f1dd265aa125ebd80c4d2fe
Alex Kim <alex@terra.money>
2020-10-29 23:03:48 +0900

    fix: add db migration code

---
+++ src/orm/migration/1603979983323-create_latest_oracle_prices_func.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateLatestOraclePrices1603979983323 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
+drop function public.latestOraclePrices;
+
+CREATE OR REPLACE FUNCTION public.latestOraclePrices("timestamp" timestamp)
+  RETURNS TABLE ("token" varchar, "price" numeric)
+  LANGUAGE 'plpgsql'
+  VOLATILE 
+AS $BODY$
+BEGIN
+
+  RETURN QUERY
+  SELECT
+    a.token,
+    (SELECT p.close FROM oracle_price p
+      WHERE p.token = a.token AND p.datetime <= "timestamp"
+      ORDER BY p.datetime DESC LIMIT 1) as price
+  FROM asset as a
+  WHERE is_listed=true;
+
+END;
+$BODY$;
+`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('drop function public.latestOraclePrices;');
+  }
+}
