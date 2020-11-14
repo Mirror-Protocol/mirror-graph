02550db788535ed07095398711d1330221dfda1e
Alex Kim <alex@terra.money>
2020-11-14 18:27:59 +0900

    fix(cli): fill price history command

--- src/cli/commands/fill.ts
+++ src/cli/commands/fill.ts
@@ -5,9 +5,8 @@ import * as bluebird from 'bluebird'
 import * as logger from 'lib/logger'
 import { fetchAggregates, TimeSpan } from 'lib/polygon'
 import { assetService, priceService } from 'services'
-import { PriceEntity, AssetEntity } from 'orm'
+import { AssetEntity, PriceEntity } from 'orm'
 import { AssetStatus } from 'types'
-import { loadDescriptions } from 'lib/data'
 
 async function collectPrice(
   symbol: string, token: string, timespan: TimeSpan, from: number, to: number, useOnlyDay = false
@@ -27,50 +26,46 @@ async function collectPrice(
   })
 }
 
-async function fillPriceHistory(): Promise<void> {
+async function fillPriceHistory(asset: AssetEntity): Promise<void> {
+  const symbol = asset.symbol.substring(1)
+  const price = await priceService().get({ token: asset.token }, { order: { id: 'ASC' }})
+
+  const minFrom = startOfDay(addDays(price.datetime, -1)).getTime()
+  const minTo = addMinutes(price.datetime, -1).getTime()
+  const hourFrom = startOfDay(addDays(minFrom, -7)).getTime()
+  const hourTo = minFrom
+  const dayFrom = startOfDay(new Date('2015-01-01')).getTime()
+  const dayTo = hourFrom
+
+  // collect ohlc of days
+  await collectPrice(symbol, asset.token, TimeSpan.DAY, dayFrom, dayTo, true)
+  // collect ohlc of hours
+  await collectPrice(symbol, asset.token, TimeSpan.HOUR, hourFrom, hourTo)
+  // collect ohlc of minutes
+  await collectPrice(symbol, asset.token, TimeSpan.MINUTE, minFrom, minTo)
+}
+
+async function fillPriceHistoryAll(): Promise<void> {
   const assets = await assetService().getAll({ where: { status: AssetStatus.LISTED }})
 
   await bluebird.mapSeries(assets, async (asset) => {
     if (asset.symbol === 'MIR')
       return
 
-    const symbol = asset.symbol.substring(1)
-
-    const price = await priceService().get(undefined, { order: { id: 'ASC' }})
-    const minFrom = startOfDay(addDays(price.datetime, -1)).getTime()
-    const minTo = addMinutes(price.datetime, -1).getTime()
-    const hourFrom = startOfDay(addDays(minFrom, -7)).getTime()
-    const hourTo = minFrom
-    const dayFrom = startOfDay(new Date('2015-01-01')).getTime()
-    const dayTo = hourFrom
-
-    // collect ohlc of days
-    await collectPrice(symbol, asset.token, TimeSpan.DAY, dayFrom, dayTo, true)
-    // collect ohlc of hours
-    await collectPrice(symbol, asset.token, TimeSpan.HOUR, hourFrom, hourTo)
-    // collect ohlc of minutes
-    await collectPrice(symbol, asset.token, TimeSpan.MINUTE, minFrom, minTo)
+    await fillPriceHistory(asset)
   })
 }
 
 export function fillCommands(): void {
   program
-    .command('fill-price-history')
-    .action(async () => {
-      await fillPriceHistory()
-
-      logger.info('completed')
-    })
-
-  program
-    .command('fill-description')
-    .action(async () => {
-      const descriptions = loadDescriptions()
-
-      await bluebird.map(Object.keys(descriptions), async (symbol) => {
-        await getRepository(AssetEntity)
-          .update({ symbol: `m${symbol}` }, { description: descriptions[symbol] })
-      })
+    .command('fill-price-history <symbol>')
+    .action(async (symbol: string) => {
+      if (symbol === 'all') {
+        await fillPriceHistoryAll()
+      } else {
+        const asset = await assetService().get({ symbol })
+        await fillPriceHistory(asset)
+      }
 
       logger.info('completed')
     })
