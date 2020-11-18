e10e6df1050278b39b2a657d0c229f67e9a6e799
Alex Kim <alex@terra.money>
2020-11-19 03:17:49 +0900

    fix: collect only 1 height

--- src/collector/collect.ts
+++ src/collector/collect.ts
@@ -21,7 +21,7 @@ export async function collect(now: number): Promise<void> {
   const collectedHeight = collectedBlock.height
 
   logger.info(`collecting ${collectedHeight + 1}-${latestHeight}`)
-  const txs = await getTxs(collectedHeight + 1, latestHeight, 10).catch(errorHandler)
+  const txs = await getTxs(collectedHeight + 1, latestHeight, 1).catch(errorHandler)
   logger.info(`txs ${txs ? txs.length : 0}`)
   if (!txs) {
     return
