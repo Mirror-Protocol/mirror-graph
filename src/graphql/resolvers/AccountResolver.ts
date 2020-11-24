0524f8241fa5faad86ae4d94b1241d0a3e38179f
Alex Kim <alex@terra.money>
2020-11-24 17:14:36 +0900

    feat: query moonpay transaction history

--- src/graphql/resolvers/AccountResolver.ts
+++ src/graphql/resolvers/AccountResolver.ts
@@ -1,14 +1,13 @@
 import { Resolver, Query, Mutation, Root, Arg, FieldResolver } from 'type-graphql'
-import { AccountService, GovService } from 'services'
+import GraphQLJSON from 'graphql-type-json'
+import { history as moonpayHistory } from 'lib/moonpay'
+import { AccountService } from 'services'
 import { Account, AssetBalance, ValueAt } from 'graphql/schema'
 import { AccountEntity } from 'orm'
 
 @Resolver((of) => Account)
 export class AccountResolver {
-  constructor(
-    private readonly accountService: AccountService,
-    private readonly govService: GovService,
-  ) {}
+  constructor(private readonly accountService: AccountService) {}
 
   @Query((returns) => Account, { nullable: true })
   async account(@Arg('address') address: string): Promise<Account> {
@@ -35,6 +34,14 @@ export class AccountResolver {
     @Arg('interval', { description: 'unit is minute' }) interval: number,
   ): Promise<ValueAt[]> {
     return this.accountService.getBalanceHistory(address, from, to, interval)
+  }
+
+  @Query((returns) => GraphQLJSON, { nullable: true })
+  async moonpayHistory(
+    @Arg('transactionId') transactionId: string,
+    @Arg('limit', { defaultValue: 1 }) limit?: number,
+  ): Promise<unknown | null> {
+    return moonpayHistory(transactionId, limit)
   }
 
   @Mutation((returns) => Account, { nullable: true })
