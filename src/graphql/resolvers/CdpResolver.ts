98864752c31dab3e764dc8f4bd93ab8f0f421b78
Alex Kim <alex@terra.money>
2020-11-29 15:13:21 +0900

    feat: add address condition in cdp query

--- src/graphql/resolvers/CdpResolver.ts
+++ src/graphql/resolvers/CdpResolver.ts
@@ -8,11 +8,16 @@ export class CdpResolver {
   constructor(private readonly cdpService: CdpService) {}
 
   @Query((returns) => [Cdp], { description: 'Get all cdps' })
-  async cdps(@Arg('maxRatio') maxRatio: number): Promise<Cdp[]> {
+  async cdps(
+    @Arg('maxRatio') maxRatio: number,
+    @Arg('address', { nullable: true }) address?: string,
+  ): Promise<Cdp[]> {
+    const addressCondition = address ? { address } : {}
     return this.cdpService.getAll({
       where: {
         collateralRatio: Raw((alias) => `${alias} < ${maxRatio}`),
-        mintAmount: MoreThan(0)
+        mintAmount: MoreThan(0),
+        ...addressCondition
       }
     })
   }
