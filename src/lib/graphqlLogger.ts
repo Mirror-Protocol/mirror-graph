bcce99f203c9cc1266a8b531375c1476a68fd051
Alex Kim <alex@terra.money>
2021-03-02 12:53:41 +0900

    fix(graphql): log

--- src/lib/graphqlLogger.ts
+++ src/lib/graphqlLogger.ts
@@ -1,7 +1,16 @@
 import { ApolloServerPlugin } from 'apollo-server-plugin-base'
 import { GraphQLRequestContext } from 'apollo-server-types'
 import { GraphQLRequestListener } from 'apollo-server-plugin-base/src/index'
+import { OperationDefinitionNode } from 'graphql'
+import { get } from 'lodash'
 import * as logger from 'lib/logger'
+
+function operationToLog(operation: OperationDefinitionNode): string {
+  return (operation.selectionSet?.selections || [])
+    .map((selection) => get(selection, 'name.value'))
+    .filter(Boolean)
+    .join(',')
+}
 
 // https://stackoverflow.com/questions/59988906/how-do-i-write-a-apollo-server-plugin-to-log-the-request-and-its-duration
 export const GraphQLLogger: ApolloServerPlugin  = {
@@ -14,20 +23,19 @@ export const GraphQLLogger: ApolloServerPlugin  = {
           return
         }
 
-        const { operation, operationName, queryHash } = context
+        const { operation, queryHash } = context
 
-        logger.info(`${queryHash.substr(-6)} ${operation?.operation} ${operationName}: ${JSON.stringify(context.request.variables)}`)
+        const variables = context.request?.variables
+          ? `: ${JSON.stringify(context.request?.variables)}`
+          : ''
+
+        logger.info(`${queryHash.substr(-6)} ${operation?.operation} ${operationToLog(operation)}${variables}`)
       },
 
       didEncounterErrors(context) {
-        const {
-          operation,
-          operationName,
-          queryHash,
-          errors,
-        } = context
+        const { operation, queryHash, errors } = context
 
-        logger.info(`${queryHash.substr(-6)} ${operation?.operation} ${operationName} error: ${JSON.stringify(errors)}`)
+        logger.info(`${queryHash.substr(-6)} ${operation?.operation} error: ${JSON.stringify(errors)}`)
       },
 
       willSendResponse(context) {
@@ -35,16 +43,12 @@ export const GraphQLLogger: ApolloServerPlugin  = {
           return
         }
 
-        const {
-          operation,
-          operationName,
-          queryHash,
-        } = context
+        const { operation, queryHash } = context
 
         const elapsed = Date.now() - start
         const size = JSON.stringify(context.response).length * 2
 
-        logger.info(`${queryHash.substr(-6)} ${operation?.operation} ${operationName} response duration=${elapsed}ms bytes=${size}`)
+        logger.info(`${queryHash.substr(-6)} ${operation?.operation} response: duration=${elapsed}ms bytes=${size}`)
       }
     }
   },
