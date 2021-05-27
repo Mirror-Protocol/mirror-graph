78cf661afb4742811226d3c03572e50803b23f1b
Alex Kim <alex@terra.money>
2021-05-27 20:36:04 +0900

    fix(bot): minor

--- src/bot/jobs/staking.ts
+++ src/bot/jobs/staking.ts
@@ -7,7 +7,7 @@ import { Updater } from 'lib/Updater'
 import { govService, assetService } from 'services'
 import { AssetStatus } from 'types'
 
-const updater = new Updater(5 * 60000) // 5mins
+const updater = new Updater(60000) // 1min
 
 export async function adjustPremium(wallet: TxWallet): Promise<void> {
   if (!updater.needUpdate(Date.now())) {
@@ -21,32 +21,30 @@ export async function adjustPremium(wallet: TxWallet): Promise<void> {
   const { premiumMinUpdateInterval } = await getStakingConfig(staking)
 
   const listeds = await assetService().getAll({ where: { status: AssetStatus.LISTED } })
-  if (listeds && listeds.length > 0) {
-    const assetTokens = await bluebird
-      .filter(listeds, (listed) => listed.symbol !== 'MIR' && listed.symbol !== 'uusd')
-      .map((listed) => listed.token)
-      .filter(async (token) => {
-        const pool = await getStakingPool(staking, token)
-        return (
-          !pool.premiumUpdatedTime ||
-          pool.premiumUpdatedTime == 0 ||
-          latestHeight >= pool.premiumUpdatedTime + premiumMinUpdateInterval
-        )
-      })
-
-    if (assetTokens && assetTokens.length > 0) {
-      const gas = 300_000 + 20_000 * (assetTokens.length - 1)
-      await wallet.execute(
-        staking,
-        { adjustPremium: { assetTokens: assetTokens } },
-        new Coins([]),
-        new StdFee(3_500_000, { uusd: gas })
-        //new StdFee(3500000, { uusd: 550000 })
+  const assetTokens = await bluebird
+    .filter(listeds, (listed) => listed.symbol !== 'MIR' && listed.symbol !== 'uusd')
+    .map((listed) => listed.token)
+    .filter(async (token) => {
+      const pool = await getStakingPool(staking, token)
+      return (
+        !pool.premiumUpdatedTime ||
+        pool.premiumUpdatedTime == 0 ||
+        latestHeight >= pool.premiumUpdatedTime + premiumMinUpdateInterval
       )
+    })
 
-      logger.info(
-        `> adjust premium count: ${assetTokens.length}, assets: ${JSON.stringify(assetTokens)}`
-      )
-    }
+  if (assetTokens && assetTokens.length > 0) {
+    const gas = 300_000 + 20_000 * (assetTokens.length - 1)
+    await wallet.execute(
+      staking,
+      { adjustPremium: { assetTokens: assetTokens } },
+      new Coins([]),
+      new StdFee(3_500_000, { uusd: gas })
+      //new StdFee(3500000, { uusd: 550000 })
+    )
+
+    logger.info(
+      `> adjust premium count: ${assetTokens.length}, assets: ${JSON.stringify(assetTokens)}`
+    )
   }
 }
