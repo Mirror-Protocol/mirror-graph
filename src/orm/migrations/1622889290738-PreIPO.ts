13c885c374595cb09bfd0005f9aa20b211519735
Alex Kim <alex@terra.money>
2021-06-05 19:38:05 +0900

    fix: bug

--- src/orm/migrations/1622889290738-PreIPO.ts
+++ src/orm/migrations/1622889290738-PreIPO.ts
@@ -10,11 +10,9 @@ export class PreIPO1622889290738 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "asset" ALTER COLUMN "status" TYPE "asset_status_enum" USING "status"::"text"::"asset_status_enum"`);
         await queryRunner.query(`ALTER TABLE "asset" ALTER COLUMN "status" SET DEFAULT 'NONE'`);
         await queryRunner.query(`DROP TYPE "asset_status_enum_old"`);
-        await queryRunner.query(`ALTER TABLE "asset_positions" ADD CONSTRAINT "FK_07222f75a8c1127264c6ad98f36" FOREIGN KEY ("token") REFERENCES "asset"("token") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "asset_positions" DROP CONSTRAINT "FK_07222f75a8c1127264c6ad98f36"`);
         await queryRunner.query(`CREATE TYPE "asset_status_enum_old" AS ENUM('NONE', 'LISTED', 'DELISTED', 'COLLATERAL')`);
         await queryRunner.query(`ALTER TABLE "asset" ALTER COLUMN "status" DROP DEFAULT`);
         await queryRunner.query(`ALTER TABLE "asset" ALTER COLUMN "status" TYPE "asset_status_enum_old" USING "status"::"text"::"asset_status_enum_old"`);
